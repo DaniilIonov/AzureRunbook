efab5880365402bbe864762c33002aa141890d03
Daniil Ionov <ionovdanil@gmail.com>
2020-06-02 18:05:45 -0400

    Cleaned-up scrips

--- WorkflowScripts/Scale-UpDown-AzureSqlElasticPool.ps1
+++ WorkflowScripts/Scale-UpDown-AzureSqlElasticPool.ps1
@@ -5,37 +5,37 @@
 		[ValidateNotNullOrEmpty()]
 		[String]
 		$subscriptionId,
-	
+
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
 		[String]
 		$resourceGroupName,
-	
+
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
 		[String]
 		$serverName,
-	
+
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
 		[String]
 		$elasticPoolName,
-	
+
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
 		[Int32]
 		$dtu,
-	
+
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
 		[Int32]
 		$databaseDtuMax,
-	
+
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
 		[Int32]
 		$databaseDtuMin,
-	
+
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
 		[Int32]
@@ -45,32 +45,32 @@
 	$ErrorActionPreference = "Stop"
 	$WarningPreference = "Continue"
 	$VerbosePreference = "Continue"
-	
+
 	"Starting..." | Write-Output
-	
+
 	# Ensures you do not inherit an AzContext in your runbook
 	$autosave = Disable-AzContextAutosave –Scope Process
-	
+
 	$connection = Get-AutomationConnection -Name AzureRunAsConnection
 	$connectionResult = Connect-AzAccount -ServicePrincipal -Tenant $connection.TenantID -ApplicationId $connection.ApplicationID -CertificateThumbprint $connection.CertificateThumbprint
-	
+
 	$context = Set-AzContext -SubscriptionId $subscriptionId
-	
+
 	$elasticPool = Get-AzSqlElasticPool -ResourceGroupName $resourceGroupName -ServerName $serverName -ElasticPoolName $elasticPoolName
-	
+
 	"Elastic Pool name: $($elasticPool.ElasticPoolName)" | Write-Output
 	"Current Elastic Pool status: $($elasticPool.State)" | Write-Output
-	
+
 	if ($null -ne $elasticPool) {
 		"Scaling the Elastic Pool: $($elasticPool.ElasticPoolName)" | Write-Output
-		
+
 		$elasticPool | Set-AzSqlElasticPool -Dtu $dtu -DatabaseDtuMax $databaseDtuMax -DatabaseDtuMin $databaseDtuMin -StorageMB $storageMB | Write-Output
-	
+
 		"Elastic Pool scaling task complete." | Write-Output
 	} 
 	else {
 		"Elastic Pool unavailable. Please retry." | Write-Error
 	}
-	
+
 	"End of script!" | Write-Output
 }
